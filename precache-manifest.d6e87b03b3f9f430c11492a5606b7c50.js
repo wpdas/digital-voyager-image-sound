abd353243e5ca5529e05576eb280f41ad65fe62e
Wenderson Pires <wpdas@yahoo.com.br>
2020-05-12 04:19:05 -0300

    Updates

---
+++ precache-manifest.d6e87b03b3f9f430c11492a5606b7c50.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a373ba7f90912357536de67c99d2685",
+    "url": "/digital-voyager-image-sound/index.html"
+  },
+  {
+    "revision": "57e0a3bd884b139a172c",
+    "url": "/digital-voyager-image-sound/static/css/2.d8e357bd.chunk.css"
+  },
+  {
+    "revision": "5e78e06b0a3f73589503",
+    "url": "/digital-voyager-image-sound/static/css/main.f986bfaa.chunk.css"
+  },
+  {
+    "revision": "57e0a3bd884b139a172c",
+    "url": "/digital-voyager-image-sound/static/js/2.3130f3b0.chunk.js"
+  },
+  {
+    "revision": "5d867c2be5a9baab80acccff09f9693c",
+    "url": "/digital-voyager-image-sound/static/js/2.3130f3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e78e06b0a3f73589503",
+    "url": "/digital-voyager-image-sound/static/js/main.e06c4d9a.chunk.js"
+  },
+  {
+    "revision": "affd3a504a587591bd7f",
+    "url": "/digital-voyager-image-sound/static/js/runtime-main.1995d11d.js"
+  },
+  {
+    "revision": "ecf81f5fb612f076cd7676267f444ba8",
+    "url": "/digital-voyager-image-sound/static/media/app_icon.ecf81f5f.svg"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/digital-voyager-image-sound/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/digital-voyager-image-sound/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/digital-voyager-image-sound/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/digital-voyager-image-sound/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/digital-voyager-image-sound/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/digital-voyager-image-sound/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "d0db360cce8c148ee74e515cd77491ba",
+    "url": "/digital-voyager-image-sound/static/media/image_preview.d0db360c.jpg"
+  }
+]);
